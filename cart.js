f1961539747ec0890882180bb75f3be87b667dad
yogesh harsana <e20cse331@bennett.edu.in>
2025-10-15 21:34:08 +0530

    fc

--- cart.js
+++ cart.js
@@ -93,7 +93,7 @@ document.addEventListener("DOMContentLoaded", function () {
       submitToSheet(); // Use 'COD' as fake payment ID
     } else {
       const razorpayOptions = {
-        key: "rzp_live_txdpgO15ZeYtHX",
+        key: "rzp_test_RTIT2YyHqlbVXt",
         amount: total * 100,
         currency: "INR",
         name: "Ayurveda Wellness",
